8c9bce773d7ccd44260570941b1f9186ef91a971
Shankar <shankar.karingula@phenompeople.com>
2020-02-06 16:34:20 +0530

    clean up

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,30 +5,30 @@ const Product = require('../models/product.model');
 exports.test = function (req, res) {
 
     //1. find() and Fiter combo
-    Product.find().sort({'price': 1}).limit(1).exec(function(err, docs){
+    Product.find().sort({ 'price': 1 }).limit(1).exec(function (err, docs) {
         console.log('Find and filter: docs' + docs);
 
     });
 
     //2. all queries in  in find 
-    Product.find({}, {'name': 'myName'}, {'price': -1}, function(err, docs){
+    Product.find({}, { 'name': 'myName' }, { 'price': -1 }, function (err, docs) {
         console.log('All in find() : docs' + docs);
 
     });
 
     var query = Product.find().where('name').
-    equals('Egg').where('price').gt('15').limit(2).select('name price');
+        equals('Egg').where('price').gt('15').limit(2).select('name price');
 
-    query.exec(function(err, docs){
+    query.exec(function (err, docs) {
         console.log('Query executed data' + docs);
     });
 
-    Product.find({}, function(err, resp) {
-        if(err) {
-            console.log('Error fetching data ', err);   
+    Product.find({}, function (err, resp) {
+        if (err) {
+            console.log('Error fetching data ', err);
         }
         modifiedResp = [];
-        resp.map(p => p.toJSON()).forEach(function(doc) {
+        resp.map(p => p.toJSON()).forEach(function (doc) {
             delete doc._id;
             delete doc.__v;
             modifiedResp.push(doc);
